f3a216d0e09f56d7302763a845518b4d36867006
Adrien Lapasset <aflapasset@gmail.com>
2021-04-15 10:19:15 +0200

    retours client 15/04

--- partials/footer.php
+++ partials/footer.php
@@ -4,5 +4,5 @@
   <a href="assets/2702_Reglement_Raynal_Roquelaure.pdf#page=2">Dotations</a>
   <a href="assets/2702_Mentions_legales.pdf">Mentions légales</a>
   <a href="assets/2702_Reglement_Raynal_Roquelaure.pdf#page=5">Politique de confidentialité</a>
-  <a href="assets/2702_Reglement_Raynal_Roquelaure.pdf">Conditions générales d'utilisation</a>
+  <a href="assets/2702_Mentions_legales.pdf">Conditions générales d'utilisation</a>
 </footer>
