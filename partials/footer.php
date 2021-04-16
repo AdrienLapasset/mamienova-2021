8ac5210b649a5e52f84589de6cf71cd022e5a94d
Adrien Lapasset <aflapasset@gmail.com>
2021-04-16 16:16:17 +0200

    hide footer link in mobile

--- partials/footer.php
+++ partials/footer.php
@@ -1,8 +1,8 @@
 <footer>
   <a href="home.php">Accueil</a>
   <a href="assets/2702_Reglement_Raynal_Roquelaure.pdf">Règlement</a>
-  <a href="assets/2702_Reglement_Raynal_Roquelaure.pdf#page=2">Dotations</a>
-  <a href="assets/2702_Mentions_legales.pdf">Mentions légales</a>
-  <a href="assets/2702_Reglement_Raynal_Roquelaure.pdf#page=5">Politique de confidentialité</a>
+  <a class="d-none d-lg-block" href="assets/2702_Reglement_Raynal_Roquelaure.pdf#page=2">Dotations</a>
+  <a class="d-none d-lg-block" href="assets/2702_Mentions_legales.pdf">Mentions légales</a>
+  <a class="d-none d-lg-block" href="assets/2702_Reglement_Raynal_Roquelaure.pdf#page=5">Politique de confidentialité</a>
   <a href="assets/2702_Mentions_legales.pdf">Conditions générales d'utilisation</a>
 </footer>
