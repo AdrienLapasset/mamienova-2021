65c01f8b1e9a4ea8245e7224068d7cf0e8adc043
Adrien Lapasset <aflapasset@gmail.com>
2021-11-24 15:03:38 +0100

    add home

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php include 'partials/header.php'; ?>
 
 <div class="layout home">
-  <button class="btn btn-primary d-block mx-auto">Jouer</button>
-  <div class="mt-auto d-flex justify-content-start align-items-center">
-    <img class="img-fluid" src="assets/imgs/home-canape.png" alt="">
-    <img class="img-fluid" src="assets/imgs/bloc-a-gagner.png" alt="">
+  <button class="btn btn-primary d-none d-md-block mx-auto mb-5 mb-xxl-0">Jouer</button>
+  <div class="mt-auto d-flex flex-column flex-xxl-row-reverse justify-content-xxl-end align-items-xxl-center">
+    <div class="align-self-center">
+      <img class="img-fluid" src="assets/imgs/bloc-a-gagner.png" alt="">
+    </div>
+    <div class="d-none d-xxl-block">
+      <img class="img-fluid" src="assets/imgs/home-canape.png" alt="">
+    </div>
+    <div class="d-none d-md-block d-xxl-none">
+      <img class="img-fluid" src="assets/imgs/home-canape-tablet.png" alt="">
+    </div>
+    <button class="btn btn-primary d-md-none mx-auto mt-5">Jouer</button>
+    <div class="d-md-none">
+      <img class="img-fluid" src="assets/imgs/home-canape-mobile.png" alt="">
+    </div>
   </div>
 </div>
 
