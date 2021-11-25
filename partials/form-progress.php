67ff7c6417e90545378809df7f537238d3b1fc7e
Adrien Lapasset <aflapasset@gmail.com>
2021-11-25 14:34:05 +0100

    fix URI

--- partials/form-progress.php
+++ partials/form-progress.php
@@ -1,21 +1,21 @@
   <div class="form-progress">
     <div class="step active">
       <div class="number-container">
-        <img class="bulle" src="../assets/imgs/bulle.png" alt="">
+        <img class="bulle" src="assets/imgs/bulle.png" alt="">
         <div class="number">1</div>
       </div>
       <div class="title">C’est parti</div>
     </div>
     <div class="step">
       <div class="number-container">
-        <img class="bulle" src="../assets/imgs/bulle.png" alt="">
+        <img class="bulle" src="assets/imgs/bulle.png" alt="">
         <div class="number">2</div>
       </div>
       <div class="title">Dis Mamie ?</div>
     </div>
     <div class="step">
       <div class="number-container">
-        <img class="bulle" src="../assets/imgs/bulle.png" alt="">
+        <img class="bulle" src="assets/imgs/bulle.png" alt="">
         <div class="number">3</div>
       </div>
       <div class="title">Inscription</div>
