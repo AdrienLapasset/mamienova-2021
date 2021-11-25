761123db4e56b3112d7bda6e2159b0ece95109e2
Adrien Lapasset <aflapasset@gmail.com>
2021-11-25 18:20:45 +0100

    add reponse 1

---
+++ reponse-1.php
@@ -0,0 +1,24 @@
+<?php include 'partials/header.php'; ?>
+
+<div class="layout reponse reponse-1">
+  <img class="brand-logo" src="assets/imgs/logo.png" alt="">
+  <div class="container d-flex flex-column">
+    <?php include 'partials/form-progress.php'; ?>
+    <h1 class="mb-5">
+      choisissez une question que vous
+      n’avez jamais osé poser à Mamie Nova !
+    </h1>
+    <div class="form-block mx-auto">
+      <p>
+        On dit que l’âge d’une dame ne se demande pas…
+        Et puis, je suis toujours aussi irrésistible !
+      </p>
+      <p>
+        Mes petits secrets pour rester dans le coup (de cuillère) : <b>du plaisir, de la gourmandise, sans jamais culpabiliser !</b>
+      </p>
+    </div>
+    <a class="btn btn-red mx-auto mt-5" href="">suivant</a>
+  </div>
+</div>
+
+<?php include 'partials/footer.php'; ?>
